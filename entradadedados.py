80652cb0289d3fc6299886d82cea5c4a48eeaed2
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-03-27 12:10:40 -0300

    Entrada de dados inicial

---
+++ entradadedados.py
@@ -0,0 +1,5 @@
+nome = input("Digite seu nome: ")
+idade = input("Digite sua idade: ")
+altura = input("Digite sua altura: ")
+
+print(f"Bem vindo, {nome} \n Sua idade: {idade} \n Sua altura: {altura}")
