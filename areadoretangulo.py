256891c416f6d70bd6045ba5d0b363a40f53586d
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-04-16 09:48:13 -0300

    area do retangulo

---
+++ areadoretangulo.py
@@ -0,0 +1,6 @@
+base = int(input("Digite a base do retângulo (cm): "))
+altura = int(input("Digite a altura do retângulo (cm): "))
+
+area = base * altura
+
+print(f"A área do retângulo é: {area} cm²")
