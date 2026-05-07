af15beda217b2767ee128500a5f1270a1977d6c6
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-05-07 09:08:23 -0300

    melhorias

--- 3_condicionais/menumediaquadradocubo.py
+++ 3_condicionais/menumediaquadradocubo.py
@@ -13,16 +13,16 @@ if (opcao == 1 or opcao == 2 or opcao==3):
 
 if opcao == 1:
     media_pond = (num1 * 2 + num2 * 3)/(2 + 3)
-    print("A média ponderada de peso 2 e 3 do seu número é {:.2f}".format(media_pond))
+    print(f"A média ponderada de peso 2 e 3 do seu número é {media_pond:.2f}")
 elif opcao == 2:
     soma = (num1 + num2) ** 2
-    print("O quadrado da soma dos dois números é {:.2f}".format(soma))
+    print(f"O quadrado da soma dos dois números é {soma:.2f}")
 elif opcao ==3:
     if num1 < num2:
         cubo1 = num1 **3
-        print("O cubo do seu menor número é {:.2f}".format(cubo1))
+        print(f"O cubo do seu menor número é {cubo1:.2f}")
     else:
          cubo2 = num2 **3
-         print("O cubo do seu menor número é {:.2f}".format(cubo2))
+         print(f"O cubo do seu menor número é {cubo2:.2f}")
 else:
     print("ERRO, opções da tabela não escolhida ")
