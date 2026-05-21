fdea34cebc5c37fc0afc9cb4f6fd3c1faff3a7e4
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-05-21 16:03:52 -0300

    Aula 04 repeticao casa 01

---
+++ 4_repeticao/casa01.py
@@ -0,0 +1,30 @@
+while True:
+    opcao = int(input("""Calculadora Simples
+    1. Soma
+    2. Subtração
+    3. Multiplicação
+    4. Divisão
+    0. Sair
+    Escolha a operação (0/1/2/3/4): """))
+
+    if opcao == 0:
+        print("Programa encerrado!")
+        break
+    elif opcao < 0 or opcao > 4:
+        print("Opção inválida!")
+        continue
+
+    num1 = float(input("Digite o primeiro número: "))
+    num2 = float(input("Digite o segundo número: "))
+
+    if opcao == 1:
+        print(f"O resultado da soma é: {num1 + num2}")
+    elif opcao == 2:
+        print(f" o resultado da subtração é:{num1 - num2}")
+    elif opcao == 3:
+        print (f"o resultado da multiplicação é:{num1*num2}")
+    elif opcao == 4:
+        if num2 != 0:
+            print(f"O resultado da divisão é: {num1 / num2}")
+        else:
+            print("Divisão por zero! Escolha outro número!")
