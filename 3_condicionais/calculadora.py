b05ab04014fe9a9619111c653bc8fea835399899
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-05-22 09:54:23 -0300

    Calculadora atualizada

--- 3_condicionais/calculadora.py
+++ 3_condicionais/calculadora.py
@@ -7,22 +7,17 @@ print("""Calculadora Simples
 opcao = int(input("Escolha a operação (1/2/3/4): "))
 num1 = float(input("Digite o primeiro número: "))
 num2 = float(input("Digite o segundo número: "))
-
-if opcao < 1 or opcao > 4:
-    print("Opção inválida!")
-elif opcao == 1:
-    resultado = num1 + num2
-    print(f"O resultado da soma é: {resultado}")
-elif opcao ==2:
-    resultado = num1 - num2
-    print(f" o resultado da subtração é:{resultado}")
-elif opcao ==3:
-    resultado = num1*num2 
-    print (f"o resultado da multiplicação é:{resultado}")
-elif opcao ==4:
+ 
+if opcao == 1:
+    print(f"O resultado da soma é: {num1 + num2}")
+elif opcao == 2:
+    print(f"O resultado da subtração é: {num1 - num2}")
+elif opcao == 3:
+    print (f"O resultado da multiplicação é: {num1 * num2}")
+elif opcao == 4:
     if num2 != 0:
-        resultado = num1 / num2
-        print(f"O resultado da divisão é: {resultado}")
+        print(f"O resultado da divisão é: {num1 / num2:.2f}")
     else:
         print("Divisão por zero! Escolha outro número!")
-     
+else:
+    print("Opção inválida!")
