8737bc5a08f773df7735b6b61942b868ce266eed
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-05-27 21:53:15 -0300

    Correção do limpar a tela

--- 4_repeticao/classe01_v2.py
+++ 4_repeticao/classe01_v2.py
@@ -3,11 +3,11 @@ Faça um programa que recebe uma senha e enquanto ela não for igual a "12345",
 ele imprime "Tente novamente". Quando a senha estiver correta, 
 o programa encerra imprimindo "Acesso liberado".
 '''
-import os # importa a biblioteca de acesso às funções do sistema operacional
 
+print('\033c', end='') #Limpa a tela do terminal sem descer uma linha
 senha = input("Senha: ") # pede a senha pela primeira vez
 while senha != "12345": # enquanto a senha for diferente de 12345
     input("Tecle ENTER para tentar novamente!") # input para imprimir a mensagem de tente novamente e esperar que o usuário tecle ENTER antes de limpar a tela
-    os.system("clear") #Limpa a tela do terminal
+    print('\033c', end='') #Limpa a tela do terminal sem descer uma linha
     senha = input("Senha: ")
 print("Acesso liberado")
