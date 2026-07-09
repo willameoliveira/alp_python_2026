65f7a16fa4e7cc89ae77039038f16dcd4213f403
Willame Oliveira <willame.oliveira@ifpi.edu.br>
2026-07-09 08:09:17 -0300

    Calculadora com loop

--- 4_repeticao/casa01.py
+++ 4_repeticao/casa01.py
@@ -14,7 +14,7 @@ while True:
         print("Programa encerrado!")
         break
     elif opcao < 0 or opcao > 4:
-        input("Opção inválida! ENTER para continuar...")
+        input("\nOpção inválida! ENTER para continuar...")
         continue
 
     num1 = float(input("Digite o primeiro número: "))
@@ -23,9 +23,9 @@ while True:
     if opcao == 1:
         print(f"O resultado da soma é: {num1 + num2}")
     elif opcao == 2:
-        print(f" o resultado da subtração é:{num1 - num2}")
+        print(f" o resultado da subtração é: {num1 - num2}")
     elif opcao == 3:
-        print (f"o resultado da multiplicação é:{num1*num2}")
+        print (f"o resultado da multiplicação é: {num1 * num2}")
     elif opcao == 4:
         if num2 != 0:
             print(f"O resultado da divisão é: {num1 / num2}")
